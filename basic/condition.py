e5b73e23e4df91db94ace6811f1a3d60e750c7e3
shallowaria <shallowaria@gmail.com>
2026-04-08 21:43:11 +0800

    1-5Python基础

--- basic/condition.py
+++ basic/condition.py
@@ -6,3 +6,28 @@ elif age >= 6:
 else:
     print('your age is', age)
     print('kid')
+
+
+age = 15
+
+match age:
+    case x if x < 10:
+        print(f'< 10 years old: {x}')
+    case 10:
+        print('10 years old.')
+    case 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18:
+        print('11~18 years old.')
+    case 19:
+        print('19 years old.')
+    case _:
+        print('not sure.')
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum) # 5050
+
+L = ['Bart', 'Lisa', 'Adam']
+
+for name in L:
+    print('Name:', name)
